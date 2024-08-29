08da4fbe1a419c01dd31aa8265c1fd2e0ced5d42
AdmiN <AdmiN@ITWS0022>
2024-08-29 09:42:11 +0530

    second commit

--- sravani532/src/sravani/com/lbrce/Sample.java
+++ sravani532/src/sravani/com/lbrce/Sample.java
@@ -6,3 +6,9 @@ public static void main(String args[])
 	System.out.println("sravani");
 }
 }
+public class Aswini{
+	public static void main(Stringargs[])
+	{
+		System.out.println("ASwini");
+	}
+}
