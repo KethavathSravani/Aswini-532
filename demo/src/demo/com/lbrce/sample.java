ba26e8292de75e7c1bee4fcb6b046e1d63cbd669
AdmiN <AdmiN@ITWS0024>
2024-08-29 10:32:22 +0530

    commit 11

--- demo/src/demo/com/lbrce/sample.java
+++ demo/src/demo/com/lbrce/sample.java
@@ -3,6 +3,7 @@ package demo.com.lbrce;
 public class sample {
 public static void main(String args[])
 {
-	System.out.println("sravani");
+	//System.out.println("sravani");
+	System.out.println("Demo edited.............");
 }
 }
